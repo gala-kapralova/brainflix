31eb51bc6f31e148499a30c24cf2df7a2179a479
Galina Kapralova <g.m.kapralova@gmail.com>
2024-01-27 17:54:08 -0800

    add <nav> tag for navigation menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,36 @@
-import logo from "../../assets/logo/BrainFlix-logo.svg";
-import avatar from "../../assets/images/Mohan-muruge.jpg";
-import "./Header.scss";
+    import logo from "../../assets/logo/BrainFlix-logo.svg";
+    import avatar from "../../assets/images/Mohan-muruge.jpg";
+    import "./Header.scss";
 
-const Header = () => (
+    const Header = () => (
     <header className="header">
-        <Logo/>
-        <Search/>
-        <Avatar/>
-        <UploadButton/>
+        <nav className="header__nav">
+            <Logo/>
+            <Search/>
+            <Avatar/>
+            <UploadButton/>
+        </nav>
     </header>
-);
+    );
 
-const Logo = () => (
+    const Logo = () => (
     <div className="header__logo">
         <a href="../../App" className="home__link"><img src={logo} alt="Logo" className="header__logo-icon"/></a>
     </div>
-);
+    );
 
-const Search = () => (
+    const Search = () => (
     <form className="header__form">
         <input type="search" placeholder="Search" className="header__search"></input>
     </form>
-);
+    );
 
-const Avatar = () => (
+    const Avatar = () => (
     <img src={avatar} alt="Avatar image" className="header__avatar" />
-)
+    )
 
-const UploadButton = () => (
+    const UploadButton = () => (
     <button className="header__button">UPLOAD</button>
-)
+    )
 
-export default Header;
+    export default Header;
