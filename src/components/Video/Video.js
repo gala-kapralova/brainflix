6a5d999ffdfea7d409fdb2dc019e5c7015b6abb9
Galina Kapralova <g.m.kapralova@gmail.com>
2024-01-28 20:52:35 -0800

    retrieve data from API suing axios, update style

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,17 +1,33 @@
 import "./Video.scss"
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import axiosInstance from "../../components/axios-instance";
 
-const Video = props => {
+export const Video = ({ id, channel, image }) => {
+
+    const [ videoDetails, setVideoDetails ] = useState({})    
+    const fetchFullVideoDetails = async () => {
+        try {
+            const response = await axiosInstance.get(`videos/${id}`);
+            setVideoDetails(response.data);
+        } catch (error) {
+            console.error('Error fetching video details:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchFullVideoDetails();
+    },[id])
+
     return (
         <section className="video">
             <div>
-                <img src={props.image} alt="Video Thumbnail" className="video__thumbnail" />
+            <Link to={`/video-player/${id}`} ><img src={image} alt="Video Thumbnail" className="video__thumbnail" /></Link>
             </div>
             <div className="video__details">
-                <p className="video__details-title">{props.title}</p>
-                <p className="video__details-channel">{props.channel}</p>
+                <Link to={`/video-player/${id}`} className="video__details-title">{videoDetails.title}</Link>
+                <p className="video__details-channel">{channel}</p>
             </div>
         </section>
     )
 }
-
-export default Video;
