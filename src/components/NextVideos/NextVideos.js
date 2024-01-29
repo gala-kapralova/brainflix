3c41e81abb289932ac86dfdd2015b9d8341e7522
Galina Kapralova <g.m.kapralova@gmail.com>
2024-01-28 21:17:09 -0800

    retrieve nextVideos data from API

--- src/components/NextVideos/NextVideos.js
+++ src/components/NextVideos/NextVideos.js
@@ -1,22 +1,38 @@
-import { Component } from "react";
-import videos from "../../data/videos.json";
-import Video from "../Video/Video";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Video } from "../Video/Video";
 import "./NextVideos.scss";
+import axiosInstance from "../axios-instance";
 
-class NextVideos extends Component {
-    render() {
-        return (
-            <section className="nextVideos">
-                <h3 className="nextVideos__title">NEXT VIDEOS</h3>
-                {videos.map((video, i) => {
-                    if (video.id !== this.props.currentVideoId) {
-                        return (<Video key={video.id} title={video.title} channel={video.channel} image={video.image} />);
-                    }
-                    return null; 
-                })}
-            </section>
-        )
-    }
-}
+export const NextVideos = ({ currentVideoId }) => {
+    const [nextVideos, setNextVideos] = useState([]);
+    const { videoId } = useParams();
 
-export default NextVideos;
+    useEffect(() => {
+        const fetchNextVideos = async () => {
+            try {
+                const response = await axiosInstance.get('videos');
+                setNextVideos(response.data);
+            } catch (error) {
+                console.error('Error fetching video list:', error);
+            }
+        };
+
+        fetchNextVideos();
+    }, []);
+
+    const videoIdToShow = videoId || currentVideoId;
+
+    return (
+        <section className="nextVideos">
+            <h3 className="nextVideos__title">NEXT VIDEOS</h3>
+            <ul>
+                {nextVideos.filter(video => video.id !== videoIdToShow).map(video => (
+                    <li key={video.id}>
+                        <Video {...video} />
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
