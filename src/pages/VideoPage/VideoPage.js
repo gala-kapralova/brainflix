cf82433042a38e46549e301ee642a7fa372c79e5
Galina Kapralova <g.m.kapralova@gmail.com>
2024-01-28 22:15:20 -0800

    add video page

--- src/pages/VideoPage/VideoPage.js
+++ src/pages/VideoPage/VideoPage.js
@@ -1,30 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Header from "../../components/Header/Header";
 import MainVideo from "../../components/MainVideo/MainVideo";
-import videos from "../../data/videos.json";
 import VideoContent from "../../components/VideoContent/VideoContent";
-import videoDetails from "../../data/video-details.json";
-import NextVideos from "../../components/NextVideos/NextVideos";
+import { NextVideos } from "../../components/NextVideos/NextVideos";
+import axiosInstance from "../../components/axios-instance";
 
 function VideoPage() {
-  const videoData = videos[0];
-  const videoContent = videoDetails[0];
+  const [nextVideos, setNextVideos] = useState([]);
+  const [currentVideo, setCurrentVideo] = useState(null);
+  const { videoId } = useParams();
+
+  useEffect(() => {
+    const fetchNextVideos = async () => {
+      try {
+        const response = await axiosInstance.get('videos');
+        setNextVideos(response.data);
+        setCurrentVideo(response.data.find(video => video.id === videoId) || response.data[0]);
+      } catch (error) {
+        console.error('Error fetching video list:', error);
+      }
+    };
+
+    fetchNextVideos();
+  }, [videoId]); 
+
+  useEffect(() => {
+    if (!currentVideo?.id) return;
+
+    const fetchVideoDetails = async () => {
+      try {
+        const response = await axiosInstance.get(`videos/${currentVideo.id}`);
+        setCurrentVideo(response.data);
+      } catch (error) {
+        console.error('Error fetching video details:', error);
+      }
+    };
+
+    fetchVideoDetails();
+  }, [currentVideo?.id]); 
+
+  if (!currentVideo) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="VideoPage">
       <Header />
-      <MainVideo image={videoData.image}/>
+      <MainVideo image={currentVideo.image} />
       <VideoContent 
-      title={videoContent.title}
-      channel={videoContent.channel}
-      image={videoContent.image}
-      description={videoContent.description}
-      views={videoContent.views}
-      likes={videoContent.likes}
-      timestamp={videoContent.timestamp}
-      comments={videoContent.comments}
-      avatar={videoContent.avatar}/>
-      <NextVideos currentVideoId={videoData.id} />
+        title={currentVideo.title}
+        channel={currentVideo.channel}
+        image={currentVideo.image}
+        description={currentVideo.description}
+        views={currentVideo.views}
+        likes={currentVideo.likes}
+        timestamp={currentVideo.timestamp}
+        comments={currentVideo.comments}
+        avatar={currentVideo.avatar}/>
+      <NextVideos nextVideos={nextVideos} currentVideoId={currentVideo.id} />
     </div>
   );
 }
 
 export default VideoPage;
+
